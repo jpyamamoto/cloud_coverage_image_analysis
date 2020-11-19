eaf002c64fc9d9bdb63b87a9495f2fabf1aad71e
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2020-11-18 19:03:59 -0600

    Docs and args

---
+++ docs/source/index.rst
@@ -0,0 +1,24 @@
+.. Cloud Coverage Image Analysis documentation master file, created by
+   sphinx-quickstart on Wed Nov 18 18:36:17 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Cloud Coverage Image Analysis: Documentation
+=========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+main
+===================
+.. automodule:: main
+   :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 """
-Module Docstring
+Cloud Coverage Analysis.
+
+Given an image whose path is received as the first argument when calling the
+program, return the Cloud Coverage Index.
+
+The Cloud Coverage Index is given by _CCI = N/C_ where _N_ stands for the
+amount of pixels classified as cloud, and _C_ is the number of pixels in
+the valid area of the given picture.
+
+An optional flag "s" or "S" may be given, which indicates that the resulting
+image after classifying every pixel should be saved to a file named:
+`image-seg.jpg`
+assuming that the given file was originally called `image.jpg`
 """
 
 __author__ = "Juan Pablo Yamamoto Zazueta and Luis Edgar Flores Ayala"
@@ -12,36 +24,23 @@ import argparse
 
 def main(args):
     """ Main entry point of the app """
-    print("hello world")
-    print(args)
+    image_path = args.image_path
+    export = bool(args.S)
+
+    print("File to be read: {}".format(image_path))
+    print("Save file: {}".format(export))
 
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog = 'main.py',
+                                     usage = '%(prog)s image_path [s|S]',
+                                     description = 'Determine the Cloud Coverage Index of the given image.')
 
-    # Required positional argument
-    parser.add_argument("arg", help="Required positional argument")
-
-    # Optional argument flag which defaults to False
-    parser.add_argument("-f", "--flag", action="store_true", default=False)
-
-    # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-n", "--name", action="store", dest="name")
-
-    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="count",
-        default=0,
-        help="Verbosity (-v, -vv, etc)")
-
-    # Specify output of "--version"
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="%(prog)s (version {version})".format(version=__version__))
+    # Positional arguments
+    parser.add_argument("image_path", help="The name of the image to analyze")
+    parser.add_argument("S", nargs="?", choices=("S", "s"), default=False,
+                        help="Passed if the output image should be saved to <image_path>-seg.jpg")
 
     args = parser.parse_args()
     main(args)
