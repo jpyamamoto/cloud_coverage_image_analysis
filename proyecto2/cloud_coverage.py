8a5c8b1cccacdd8a0c38d69ac4230796a159e8c7
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2020-11-22 20:50:38 -0600

    Eficiente

--- proyecto2/cloud_coverage.py
+++ proyecto2/cloud_coverage.py
@@ -10,6 +10,7 @@ class CloudCoverage:
         total_row , total_col , _ = image.shape
         x , y = np.ogrid[:total_row , :total_col]
         cen_x , cen_y = total_row/2 , total_col/2
+
         distance_from_the_center = np.sqrt((x-cen_x)**2 + (y-cen_y)**2)
         outer_mask = distance_from_the_center > cls.RADIUS
 
@@ -25,21 +26,13 @@ class CloudCoverage:
 
     @classmethod
     def classify(cls, image):
-        for i in range(len(image)):
-            for j in range(len(image[i])):
-                if not image[i][j].any():
-                    continue
+        blue, red, alpha = image[:, :, 0], image[:, :, 2], image[:, :, 3]
 
-                red = image[i][j][2]
-                blue = image[i][j][0]
+        ratios = np.divide(red, blue, out=np.ones(red.shape, dtype=float), where=blue != 0)
+        mask = ratios >= 0.95
 
-                if blue == 0:
-                    blue = 0.0000001
-
-                if (red / blue) < 0.95:
-                    image[i][j] = [0,0,0,255]
-                else:
-                    image[i][j] = [255,255,255,255]
+        image[(alpha != 0) & mask] = [255,255,255,255]
+        image[(alpha != 0) & ~mask] = [0,0,0,255]
 
         return image
 
